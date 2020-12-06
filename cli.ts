72a391480d543592094f9084decfc113705c0995
h666t <1845720928@qq.com>
2020-12-06 20:16:41 +0800

    finish db.read

---
+++ cli.ts
@@ -0,0 +1,28 @@
+const { program } = require('commander');
+const pkg = require('./package.json')
+const db = require('./db.ts')
+let path = 'public'
+let port = '8888'
+let cacheControl = '0'
+
+program
+  .version(pkg.version)
+  // .option('-port, --chdir <path>', 'change the working directory')
+
+// 不传参
+if (process.argv.length === 2 ){
+  //读取homedir/.serveur的参数
+  db.read().then((content)=>{
+    if (content === ''){
+      console.log('n');
+    }
+  })
+}
+
+
+
+console.log(process.argv);
+
+
+
+program.parse(process.argv);
---
+++ db.ts
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const p = require('path');
+const homedir = process.env.HOME || require('os').homedir();
+const argsPath = p.join(homedir, '.serveur');
+
+const db = {
+  read() {
+    return new Promise((resolve, reject)=>{
+      fs.readFile(argsPath, {flag: 'a+'}, (err, data) => {
+        if (err) return reject(err);
+        let content = data.toString();
+        resolve(content);
+      });
+    })
+  }
+};
+
+module.exports = db;

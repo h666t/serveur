7d3bebd137d3a2832b87d6cb41d91fb617f58464
h666t <1845720928@qq.com>
2020-12-06 19:44:41 +0800

    ready for commander

--- serveur.ts
+++ serveur.ts
@@ -1,9 +1,11 @@
 import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
 
 const serveur = http.createServer()
 
-serveur.on('request',()=>{
+serveur.on('request',(request:IncomingMessage,response:ServerResponse)=>{
   console.log('hello world');
+  response.end('hi')
 })
 
 serveur.listen('8888')

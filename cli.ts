09cb331458edf1c863e9d9231684f09a1af6fe99
h666t <1845720928@qq.com>
2020-12-07 09:32:35 +0800

    can run cli commander through node

--- cli.ts
+++ cli.ts
@@ -4,27 +4,31 @@ const db = require('./db.ts');
 let path = 'public';
 let port = '8888';
 let cacheControl = '0';
-
+const {exec} = require("child_process");
+//通过exec可以执行命令行命令
 program
-  .version(pkg.version);
-// .option('-port, --chdir <path>', 'change the working directory')
+  .version(pkg.version)
+  .option('-p, --port <port>', 'change the port (default:8888)')
 
 // 不传参
 if (process.argv.length === 2) {
   //读取homedir/.serveur的参数
   db.read().then(function (content) {
-    if (content.length === 0) {
-      db.write([path, port, cacheControl])
-        .catch(() => {});
+    if (!content.path && !content.port && !content.cacheControl) {
+      db.write({path,port,cacheControl})
+        .catch(err => err ? console.log(err) : undefined);
+      exec('tsnd serveur.ts')
       return;
-    }
-    if (content.length === 3) {
-
     }
     throw new Error('请传入正确的参数 [path,prot,cacheControl]');
   });
+}
+
+//传4个参数 即类似于['D:\\Software\\nodejs\\node.exe', 'D:\\code\\p\\Item\\serveur\\cli.ts','-p','8888']
+if (process.argv.length === 4){
 
 }
+
 
 program.parse(process.argv);
 

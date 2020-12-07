8ac1fc87b0886e97a76893c36dd013f666a6c7ba
h666t <1845720928@qq.com>
2020-12-07 09:51:36 +0800

    db.complete under construction

--- cli.ts
+++ cli.ts
@@ -1,9 +1,7 @@
 const {program} = require('commander');
 const pkg = require('./package.json');
 const db = require('./db.ts');
-let path = 'public';
-let port = '8888';
-let cacheControl = '0';
+
 const {exec} = require("child_process");
 //通过exec可以执行命令行命令
 program
@@ -12,21 +10,17 @@ program
 
 // 不传参
 if (process.argv.length === 2) {
-  //读取homedir/.serveur的参数
-  db.read().then(function (content) {
-    if (!content.path && !content.port && !content.cacheControl) {
-      db.write({path,port,cacheControl})
-        .catch(err => err ? console.log(err) : undefined);
-      exec('tsnd serveur.ts')
-      return;
-    }
-    throw new Error('请传入正确的参数 [path,prot,cacheControl]');
-  });
+  exec('tsnd serveur.ts')
 }
 
 //传4个参数 即类似于['D:\\Software\\nodejs\\node.exe', 'D:\\code\\p\\Item\\serveur\\cli.ts','-p','8888']
 if (process.argv.length === 4){
+    const opt = process.argv
+  if (opt[2] === '-p'){
+    db.read().then((content)=>{
 
+    })
+  }
 }
 
 
--- db.ts
+++ db.ts
@@ -21,6 +21,36 @@ const db = {
         reject(error)
       })
     })
+  },
+  complete(){
+    return new Promise((resolve, reject)=>{
+      const hash = {
+        path:"public",
+        port:"8888",
+        cacheControl:"0"
+      }
+
+      //读取homedir/.serveur的参数
+      this.read().then(function (content) {
+        const newContent = JSON.parse(JSON.parse(content))
+        for (let key in hash){
+            if (!content[key]){
+
+            }
+        }
+
+
+        if (!content.path && !content.port && !content.cacheControl) {
+          //写入默认的参数{path:"public",port:"8888",cacheControl:"0"}
+          db.write({path,port,cacheControl})
+            .catch(err => err ? console.log(err) : undefined);
+          resolve()
+          return;
+        }
+        throw new Error('请传入正确的参数 [path,port,cacheControl]');
+      });
+    })
+
   }
 
 };

e9d84337fa566a68abbb69bf41b9e0d61baf847d
h666t <1845720928@qq.com>
2020-12-13 13:57:39 +0800

    fix ico

--- serveur.ts
+++ serveur.ts
@@ -8,7 +8,7 @@ const argsPath = p.resolve(homedir, '.serveur');
 
 const dbData = JSON.parse(fs.readFileSync(argsPath).toString())
 const {path,port,cacheControl} = dbData
-const targetPath = p.join('..','..',path)
+const targetPath = p.join('..',path)
 
 const serveur = http.createServer()
 
@@ -21,9 +21,14 @@ const mangeError = (response: ServerResponse,err) => {
 const fetchPage = (request: IncomingMessage,response: ServerResponse) => {
   const filename = request.url.replace(/\//,'')
   const fileStyle = request.url.replace(/^.*\./,'')
+  if (filename === "favicon.ico"){
+    fs.readFile('./favicon/favicon.icon',(err,data)=>{
+      response.end(data)
+    })
+    return
+  }
   fs.readFile(p.resolve(targetPath,filename),(err,data)=>{
     if (err) return mangeError(response,err)
-
     response.setHeader('content-type',`text/${fileStyle};charset=uft-8`)
     response.setHeader('Cache-Control',`public,max-age=${cacheControl}`);
     response.end(data)
